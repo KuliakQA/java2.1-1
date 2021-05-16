6a31aeaf946682149ac894c881ae3e980b4fba3c
KuliakQA <culeacvadim@gmail.com>
2021-05-16 06:55:04 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1000; // в рублях
+        int bonus = 20; // 1 миля
+        int result = ticketPrice / bonus; //50 бонусных миль
+        System.out.println(result);
+    }
+}
+
+
